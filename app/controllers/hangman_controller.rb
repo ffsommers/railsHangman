32d681d6c6840bf8d26971dd184cb50bea214886
ffsommers <ssommers21@gmail.com>
2017-02-21 00:31:52 -0800

    adds top score user persistence. still need to access it for leader board.

--- app/controllers/hangman_controller.rb
+++ app/controllers/hangman_controller.rb
@@ -13,9 +13,13 @@ class HangmanController < ApplicationController
     @user = User.find(current_user.id)
     @past_top_score = @user.top_score
     p post_params[:top_score]
-    if post_params[:top_score ].to_i> @past_top_score
-      p "yipeeeee"
+    if post_params[:top_score ].to_i > @past_top_score
+      @user.top_score = post_params[:top_score].to_i
+      @user.save
+    else
+      p "not good enough"
     end
+    redirect_to games_path
     # p post_params.top_score
     # # @top_scores = User.find(:all, :order => "id desc", :limit => 5)
     # @top_scores = User.by_score.limit(3)
