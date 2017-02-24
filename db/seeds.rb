45388328541b782f0b67fb18492569976f8260ed
ffsommers <ssommers21@gmail.com>
2017-02-23 16:24:14 -0800

    fixes typo in seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
     User.create(username: 'Blake', email: 'blakes@uber.com', password:"blake", top_score: 10);
     User.create(username: 'Skip', email: 'ssommers21@gmail.com', password:"skip", top_score: 20);
-    User.create(username: 'Mom', email: 'adrienne@a3nstrategies.com ', password:"mom", top_score: 16);
+    User.create(username: 'Mom', email: 'adrienne@a3nstrategies.com', password:"mom", top_score: 16);
     User.create(username: 'Nick', email: 'nicholasreis@gmail.com', password:"nick", top_score: 7);
     User.create(username: 'Kachi', email: 'kachi@dronedeploy.com ', password:"kachi", top_score: 4);
